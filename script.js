874a20bfad2a32ed2b2f84026f910f10e7ab9104
flaviaahvs <161638358+flaviaahvs@users.noreply.github.com>
2025-03-25 14:30:11 -0300

    25-03

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
 
-    const alternaContraste
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
 
@@ -23,5 +23,9 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+alternaContraste.addEventListener('click', function(){
+
+})
 
 })
